757e75ed6891dcf46b7a5d57bd948875520d2567
GaneshkumarMurali <ganeshkumarmurali@gmail.com>
2018-10-04 21:35:19 +0530

    added the project files

---
+++ background.js
@@ -0,0 +1,59 @@
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse){
+        try{
+            if(request.msg == "startFunc") {
+                console.log('Attachment');
+                attachmentAPICall(request.image,request.ticketID);
+            }
+            else if(request.popupOpen){
+                chrome.tabs.captureVisibleTab(function(url){
+                    chrome.extension.sendMessage({method: 'setScreenshotUrl',data: url},function(response) {});
+                });
+            }
+        }
+        catch(ex) {
+            console.log(ex);
+        }
+    }
+);
+
+function attachmentAPICall(dataURI, sysId){
+    try{
+        var BASE64_MARKER =';base64,';
+        var base64Index = dataURI.indexOf(BASE64_MARKER)+BASE64_MARKER.length;
+        var base64 = dataURI.substring(base64Index);
+        var raw = window.atob(base64);
+        var rawLength=raw.length;
+        var array = new Uint8Array(new ArrayBuffer(rawLength));
+        for(i=0;i<rawLength;i++){
+            array[i]=raw.charCodeAt(i);
+        }
+        var requestBody=array;
+        var client=new XMLHttpRequest();
+        client.open("post","https://dev12176.service-now.com/api/now/attachment/file?table_name=incident&table_sys_id=" + sysId + "&file_name=Screenshot_"+sysId+".jpg");
+        client.setRequestHeader('Accept','application/json');
+        client.setRequestHeader('Content-Type', 'image/jpeg');
+        client.setRequestHeader('Authorization','Basic '+btoa('admin'+':'+'Snow!123'));
+        client.onreadystatechange=function(){
+            if(this.readyState == this.DONE){
+                var httpResponse = JSON.parse(this.response);
+            }
+        };
+        client.send(requestBody);
+    }
+    catch(ex) {
+        console.log(ex);
+    }
+}
+
+// function takeScreenshot(sysId){
+//     chrome.tabs.captureVisibleTab(function (screenshotUrl){
+//         console.log(screenshotUrl);
+//         try{
+//             attachmentAPICall(screenshotUrl,sysId);
+//         }
+//         catch(ex) {
+//             console.log(ex);
+//         }
+//     });
+// };
---
+++ popup.js
@@ -0,0 +1,52 @@
+chrome.runtime.sendMessage({popupOpen: true});
+var screen;
+chrome.extension.onMessage.addListener(function(request,sender,sendResponse){
+    switch(request.method){
+        case 'setScreenshotUrl':
+        document.getElementById('target').src = request.data;
+        sendResponse({result: 'success'});
+        screen=request.data;
+    }
+});
+var btn=document.getElementById('createBtn');
+if(btn){
+    console.log('Button is loaded');
+    btn.addEventListener('click',serviceNowAPICall);
+}
+
+function serviceNowAPICall(){
+    var callerId = document.getElementById('userid').value;
+    var shortDescription = document.getElementById('description').value;
+    if(callerId =="" || shortDescription == ""){
+        document.getElementById('message').innerHTML="User ID and Issue Description are required fields."
+        return;
+    }
+    else{
+        chrome.tabs.query({active:true, lastFocusedWindow: true}, function(tabs){
+            document.getElementById('createBtn').disabled=true;
+            var tab = tabs[0];
+            var tabURL=tab.url;
+            document.getElementById('response').innerHTML="Please wait,, ticket is being created";
+            var requestBody="{\"assignment_group\":\"Chrome Extension\",\"caller_id\":\""+callerId+"\",\"short_description\":\""+shortDescription+"\",\"work_notes\":\""+"Application URL is:"+tabURL+"\"}";
+            try{
+                var client=new XMLHttpRequest();
+                client.open("post","https://dev12176.service-now.com/api/now/table/incident?sysparm_fields=number%2Csys_id&sysparm_limit=1");
+                client.setRequestHeader('Accept','application/json');
+                client.setRequestHeader('Content-Type', 'application/json');
+                client.setRequestHeader('Authorization','Basic '+btoa('admin'+':'+'Snow!123'));
+                client.onreadystatechange=function(){
+                    if(this.readyState==this.DONE){
+                        var httpResponse = JSON.parse(this.response);
+                        document.getElementById('response').innerHTML="Your Incident number is :" +httpResponse["result"].number;
+                        var sysID = httpResponse["result"].sys_id;
+                        chrome.runtime.sendMessage({msg: "startFunc", image: screen, ticketID: sysID});
+                    }
+                };
+                client.send(requestBody);
+            }
+            catch(ex){
+                document.getElementById('message').innerHTML="An erroe occurred. Please try again later"
+            }                                                                         
+        });
+    }
+}
